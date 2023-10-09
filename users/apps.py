2de4de42afa17981e43ade84ff6303ca0fcee438
kariukijoni <kariukijoni@gmail.com>
2023-10-09 11:49:58 +0300

    Added profile_signals

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+# signals
+
+
+def ready(self):
+    import users.signals
